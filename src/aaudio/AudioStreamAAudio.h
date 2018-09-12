89c2d812ddf756bf9b5aa6f7b3ff717904cf690c
Don Turner <donturner@google.com>
2018-09-12 16:06:37 +0100

    Adding simpler getTimestamp method. Fixes #209

--- src/aaudio/AudioStreamAAudio.h
+++ src/aaudio/AudioStreamAAudio.h
@@ -87,6 +87,8 @@ public:
                                        int64_t *framePosition,
                                        int64_t *timeNanoseconds) override;
 
+    ResultWithValue<FrameTimestamp> getTimestamp(clockid_t clockId) override;
+
     StreamState getState() override;
 
     AudioApi getAudioApi() const override {
